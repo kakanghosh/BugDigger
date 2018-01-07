d74ece7aae293d53db3adb15329dab2ba98255cb
kakanghosh <kakanghosh69@gmail.com>
2018-01-07 23:06:30 +0600

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>{{$title}}</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css')}}/app.css">
+</head>
+<body>
+	<h1>This is the Home Page</h1>
+	<script type="text/javascript" src="{{asset('js')}}/app.js"></script>
+</body>
+</html>
